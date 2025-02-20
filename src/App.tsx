91fbe9dce5abcb0d77936d018717776ed4ac1f08
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2025-02-20 23:37:44 +0100

    Add: lucide, shadcn and theme mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ModeToggle } from "./components/ModeToggle"
+import { ThemeProvider } from "./context/ThemeProvider"
+
 function App() {
 
   return (
     <>
-      <h1>Potter App</h1>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1 className="text-3xl">Potter App</h1>
+        <ModeToggle/>
+      </ThemeProvider>
     </>
   )
 }
