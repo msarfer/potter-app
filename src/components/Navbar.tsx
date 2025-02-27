cc8d50d26632531c88e2160453f723cab9a99f39
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2025-02-27 17:38:59 +0100

    Añadido logica para active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,44 @@
-import { ModeToggle } from "@/components/ModeToggle"
-import { WandSparkles } from "lucide-react"
-import { ReactNode } from "react"
-import { NavLink } from "react-router-dom"
-
+import { ModeToggle } from "@/components/ModeToggle";
+import { WandSparkles } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 interface Element {
-  path: string
-  children: ReactNode
-  hide?: boolean
+  path: string;
+  name: string;
+  hide?: boolean;
 }
 
 const elements: Element[] = [
-  {path: '/books', children: <h1>Books</h1>},
-  {path: '/characters', children: <h1>Characters</h1>},
-  {path: '/spells', children: <h1>Spells</h1>},
-  {path: '/dashboard', children: <h1>Dashboard</h1>},
-  {path: '/', children: <h1>Sign in</h1>},
-  {path: '/', children: <h1>Sign up</h1>},
-  {path: '/', children: <h1>Log out</h1>}
-]
+  { path: "/books", name: "Books" },
+  { path: "/characters", name: "Characters" },
+  { path: "/spells", name: "Spells" },
+  { path: "/dashboard", name: "Dashboard" },
+];
 
-const NavElement = ({ path, children }: Element) => {
-  return <NavLink to={`${path}`} className="group pointer inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:underline focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50">{children}</NavLink>
-}
+const NavElement = ({ path, name }: Element) => {
+  return (
+    <NavLink
+      to={`${path}`}
+      className={({isActive}) => `group pointer inline-flex h-9 w-max items-center justify-center px-4 py-2 text-sm font-medium hover:underline focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50 ${isActive ? 'underline': ''}`}
+    >
+      {name}
+    </NavLink>
+  );
+};
 
 export function Navbar() {
   return (
-    <header className="flex h-1/10 w-full shrink-0 items-center px-4">
-      <NavLink to="/" className="mr-6 flex text-2xl" >
+    <header className="flex h-1/10 w-full shrink-0 items-center px-4 border border-b-1">
+      <NavLink to="/" className="mr-6 flex text-2xl">
         <WandSparkles className="h-8 w-8 mr-3" />
         <h1 className="">Potter App</h1>
       </NavLink>
       <nav className="ml-auto flex gap-6">
-        {elements.map(({path, children}, index) => <NavElement key={index + path} path={path}>{children}</NavElement>)}
-        <ModeToggle/>
+        {elements.map(({ path, name }, index) => (
+          <NavElement key={index + path} path={path} name={name} />
+        ))}
+        <ModeToggle />
       </nav>
     </header>
-  )
+  );
 }
