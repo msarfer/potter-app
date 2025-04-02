d95f9fd097fdf68ab4eb374b1e377d5e383fc4f5
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2025-04-02 17:36:35 +0200

    Obtener libros desde la api, localizada

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,16 +1,26 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import type { BookInterface } from "@/entities/potterApi"
-import { useAppSelector } from "@/hooks/useRtk"
+import { useAppDispatch, useAppSelector } from "@/hooks/useRtk"
+import { LanguageContext } from "@/providers/LanguageProvider"
+import { addFav, fetchBooks } from "@/store/features/books/booksSlice"
 import { Heart } from "lucide-react"
+import { useContext, useEffect } from "react"
 
 export default function BooksPage() {
-  const books = useAppSelector(state => state.books.books)
+  const { books, error } = useAppSelector(state => state.books)
+  const { locale } = useContext(LanguageContext)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchBooks(locale))
+  }, [locale])
   
   return (
     <section className="flex flex-wrap justify-center w-full h-full gap-x-12 gap-y-4">
+      <span>{JSON.stringify(error)}</span>
       {
-        books.map((book) => <Book key={book.index}  book={book}/>)
+        books.map((book) => <Book key={book.index} book={book}/>)
       }
     </section>
   )
@@ -21,10 +31,8 @@ interface BookProps {
 }
 
 export const Book = ({ book }: BookProps) => {
-
-  const handleFav = (id: number) => {
-    console.log(id)
-  }
+  const { favs } = useAppSelector(state => state.books)
+  const dispatch = useAppDispatch()
 
   return (
     <Card className="p-0 m-0 flex flex-row overflow-hidden w-1/3">
@@ -42,8 +50,8 @@ export const Book = ({ book }: BookProps) => {
           <div className="h-1/4 pt-2">
             <h5>Publicación: <span>{book.releaseDate}</span></h5>
             <h5>Páginas: <span>{book.pages}</span></h5>
-            <Button className="cursor-pointer" onClick={() => handleFav(book.index)}>
-              <Heart fill={`${false ? 'currentColor': 'none'}`}/>
+            <Button className="cursor-pointer" onClick={() => dispatch(addFav(book.index))}>
+              <Heart fill={`${favs.includes(book.index) ? 'currentColor': 'none'}`}/>
             </Button>
           </div>
         </CardContent>
--- src/store/features/books/booksSlice.ts
+++ src/store/features/books/booksSlice.ts
@@ -1,27 +1,79 @@
-import { BookInterface } from "@/entities/potterApi"
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import jsonBooks from '@/mocks/books.json'
+import { BookInterface } from "@/entities/potterApi";
+import {
+  createAsyncThunk,
+  createSlice,
+  PayloadAction,
+  SerializedError,
+} from "@reduxjs/toolkit";
+import { VITE_POTTER_BASE } from "@/services/config";
 
 export interface BookState {
-  books: BookInterface[],
-  favs: number[]
+  books: BookInterface[];
+  favs: number[];
+  loading: boolean;
+  error: SerializedError | null;
 }
 
 const initialState: BookState = {
-  books: jsonBooks,
-  favs: []
-}
+  books: [],
+  favs: [],
+  loading: false,
+  error: null,
+};
+
+export const fetchBooks = createAsyncThunk(
+  "books/fetch",
+  async (locale: string, { rejectWithValue }) => {
+    return new Promise<BookInterface[]>((resolve, reject) => {
+      return fetch(`${VITE_POTTER_BASE}/${locale}/books`)
+        .then((response) => {
+          if (!response.ok) {
+            reject(new Error(`Failed to fetch, status code: [${response.status}]`));
+          }else {
+            return response.json();
+          }
+        })
+        .then((books) => resolve(books))
+        .catch((error) => rejectWithValue(error.message));
+    }).catch((error) => rejectWithValue(error.message));
+  }
+);
 
 const housesSlice = createSlice({
   name: "books",
   initialState,
   reducers: {
     addFav: (state, action: PayloadAction<number>) => {
-      state.favs.push(action.payload)
-    }
-  }
-})
+      const aux = [...state.favs];
+      aux.push(action.payload);
+      state.favs = aux;
+      console.log(state.favs);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchBooks.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(
+      fetchBooks.fulfilled,
+      (state, action: PayloadAction<BookInterface[]>) => {
+        state.books = action.payload;
+        state.loading = false;
+        state.error = null;
+      }
+    );
+    builder.addCase(
+      fetchBooks.rejected,
+      (state, action: PayloadAction<SerializedError>) => {
+        console.log(action)
+        state.loading = false;
+        state.error = action.payload;
+      }
+    );
+  },
+});
 
-export const { addFav } = housesSlice.actions
+export const { addFav } = housesSlice.actions;
 
-export default housesSlice.reducer
+export default housesSlice.reducer;
