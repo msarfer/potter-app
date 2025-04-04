49206098dcceac52372de23ca5a205557a447c84
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2025-04-05 00:14:38 +0200

    Tipado función composición de proveedores

--- src/services/providers.tsx
+++ src/services/providers.tsx
@@ -1,14 +1,23 @@
-export const  buildProvidersTree = (componentsWithProps) => {
-  const initialComponent = ({ children }) => <>{children}</>
+import React from "react";
+
+interface Params {
+  children: React.ReactNode;
+}
+
+type ProviderWithProps = [React.FC<any>, Record<string, unknown>?];
+
+export const buildProvidersTree = (componentsWithProps: ProviderWithProps[]) => {
+  const initialComponent: React.FC<Params> = ({ children }: Params) => <>{children}</>;
   return componentsWithProps.reduce(
     (AccumulatedComponents, [Provider, props = {}]) => {
-      return ({ children }) => {
+      return ({ children }: Params) => {
         return (
           <AccumulatedComponents>
             <Provider {...props}>{children}</Provider>
           </AccumulatedComponents>
-        )
-      }
-    }
-  , initialComponent)
-}
+        );
+      };
+    },
+    initialComponent
+  );
+};
